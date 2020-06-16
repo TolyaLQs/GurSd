4fdc15d32d8de32e67c732dd05b001eb81e06ec1
Владик <lotarev1999@inbox.ru>
2020-06-16 18:56:07 +0500

    tolyan_g

--- static/js/news/news_script.js
+++ static/js/news/news_script.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     let forumMenuSvg = document.querySelector(".forum_menu_svg_320px");
-    let popUpWindowForum = document.querySelector(".pop_up_window_forum_320px");
+    let popUpWindowForum = document.querySelector(".pop_up_window_news_320px");
     let menuLogotypeQuitForum = document.querySelector(".menu_logotype_quit_forum");
     let contentNews = document.querySelector(".content_news");
 
